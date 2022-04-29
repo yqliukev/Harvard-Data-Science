2318f442a5bcdf2e1e6a8be88d8f9f284be94623
yqliukev <yqliukev@gmail.com>
2022-04-28 22:03:01 -0400

    Finalize Basic Data Setup

--- US-Gun-Murders/Base Data.r
+++ US-Gun-Murders/Base Data.r
@@ -1,11 +1,14 @@
+## Can be run again with mutated murders sets
+
 ## Preliminary setup
 data(murders)
-head(murders)
+intro <- head(murders)
+structure <- str(murders)
 
 ## Sorting murder counts
 ranks <- order(murders$total)
 totals <- sort(murders$total)
 
 ## Making max variables
-max_i <- 
-max <- c()
+max_i <- which.max(murders$total)
+max_murders <- c(murders$state[max_i], max(murders$total))
